1f700cb6e3a9e6211739e48ec330776b626b6b35
vyhoang <vyht444@gmail.com>
2021-02-04 11:25:19 -0600

    updated comment lines

--- propertyTax.cpp
+++ propertyTax.cpp
@@ -1,23 +1,30 @@
-// This program calculates and displays property tax of assessed value
+/*  Written by: Vy Hoang
+    Program Status: Complete
+    Program: Property Tax
+    This program calculates the annual property tax.
+*/
 
 #include <iostream>
-#include <iomanip>
+#include <iomanip>      // Header file needed to use stream manipulators
 
 using namespace std;
 int main()
 {
-    double actualValue, assessedValue, taxRate, propertyTax;
-    const double ASSESSMENT_RATE = 0.6;
+    double actualValue, assessedValue, taxRate, propertyTax;    // Declare variables
+    const double ASSESSMENT_RATE = 0.6;     // Set constant tax assessment with 60%
 
+    // Get user input the actual value and tax rate
     cout << "Enter the actual property value: $ ";
     cin >> actualValue;
     cout << "Enter the amount of tax per $100 of assessed value: $";
     cin >> taxRate;
     cout << endl;
 
+    // Calculate the assessed value and property tax.
     assessedValue = ASSESSMENT_RATE * actualValue;
     propertyTax = taxRate * assessedValue;
 
+    // Display the actual value, assessed value and property tax.
     cout << fixed << showpoint << setprecision(2);
     cout << left << setw(25) << "Actual Property Value:"
          << right << setw(4) << "$" << setw(12) << actualValue << endl;
